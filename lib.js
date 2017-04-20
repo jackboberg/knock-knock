2a066234284c77630ae3833e59c692fc509e2713
Jack Boberg <hello@jackboberg.info>
2017-04-20 13:49:01 -0400

    feat: Extract command runner
    - uses .trim instead of a regex
    - add support for stderr

--- lib.js
+++ lib.js
@@ -1,3 +1,4 @@
+const { exec } = require('child_process')
 const ReadPkgUp = require('read-pkg-up')
 
 // yield name and version from closest package.json, or empty object
@@ -12,4 +13,12 @@ exports.project = (cwd, done) => {
   const rejected = () => done(null, {})
 
   ReadPkgUp({ cwd }).then(resolved, rejected)
+}
+
+// execute command and yield trimmed output
+exports.command = (cmd, done) => {
+  exec(cmd, (err, stdout, stderr) => {
+    if (err || stderr) done(null, err ? err.toString() : stderr.trim())
+    else done(null, stdout ? stdout.trim() : '')
+  })
 }
